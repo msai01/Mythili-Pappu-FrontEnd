fac47c56de89b05e08b8316eb97e170167a7613e
msai01 <135174020+msai01@users.noreply.github.com>
2023-06-01 09:20:43 -0500

    Add files via upload

---
+++ Index2.js
@@ -0,0 +1,17 @@
+// Create a profile object
+var profile = {
+    Firstname: "Mythili",
+    Lastname: "Pappu",
+    age: 27,
+    occupation: "Java Developer",
+    location: "Texas",
+    education: "Master's in Wilmington University",
+    skills: ["HTML", "CSS", "JavaScript", "React"],
+    hobbies: ["Reading", "Cooking", "Hiking", "Photography", "Singing"],
+    email: "mythsai01@gmail.com",
+    phone: "+1234567890"
+  };
+  
+  // Display one hobby from the hobbies array in console
+  console.log("One of my hobbies is: " + profile.hobbies[profile.hobbies.length-1]);
+  
